066dea7f64839411a536d5374e4c003448c0167a
Anus Younus <AnusYounus254@gmail.com>
2024-02-27 23:00:10 +0500

    save javascript essigment 1

---
+++ Desktop/essigment/app.js
@@ -0,0 +1,74 @@
+var a = 13;
+var b = 9;
+var result1 = a + b;
+var result2 = a - b;
+var result3 = a * b;
+var result4 = a / b;
+var result5 = a % b;
+console.log(result1);
+console.log(result2);
+console.log(result3);
+console.log(result4);
+console.log(result5);
+
+var expression = 3;
+var expressionResult1 = expression + 1;
+var expressionResult2 = expressionResult1 + 11;
+var expressionResult3 = expressionResult2 - 10;
+var expressionResult4 = expressionResult3 % 4;
+
+console.log(expressionResult4);
+
+var ticket = 600;
+var ticketResult = 600 * 5;
+console.log(ticketResult);
+
+var table = 4;
+console.log(table + " X" + " 1" + " = " + table * 1);
+console.log(table + " X" + " 2" + " = " + table * 2);
+console.log(table + " X" + " 3" + " = " + table * 3);
+console.log(table + " X" + " 4" + " = " + table * 4);
+console.log(table + " X" + " 5" + " = " + table * 5);
+console.log(table + " X" + " 6" + " = " + table * 6);
+console.log(table + " X" + " 7" + " = " + table * 7);
+console.log(table + " X" + " 8" + " = " + table * 8);
+console.log(table + " X" + " 9" + " = " + table * 9);
+console.log(table + " X" + " 10" + " = " + table * 10);
+
+var item1Price = 560;
+var item1Quantity = 2;
+var totalItem1 = item1Price * item1Quantity;
+var item2Price = 200;
+var item2Quantity = 5;
+var totalItem2 = item2Price * item2Quantity;
+console.log(totalItem1 + totalItem2 + 150);
+
+var usDollar = 10 * 104;
+var saudiRiyal = 25 * 28;
+console.log(usDollar + saudiRiyal);
+
+var addition = (5 * 10) / 2;
+console.log(addition);
+
+var currentYear = 2015;
+var birthYear = 1992;
+
+console.log(currentYear - birthYear);
+
+var c = 10;
+var d = c + ++c + ++c + --c + c--;
+console.log(c);
+
+var e = 2,
+  f = 1;
+var result = --e - --f + ++f + f--;
+
+console.log(--a + --a - --b + --a - --b + ++b + --a - --b + ++b + b--);
+
+var favoriteSnake = "oreoBiscuit";
+var currentAge = 15;
+var estimateMaximum = 85;
+var amountOfsnacks = 2;
+
+var snakeResult = currentAge + estimateMaximum * amountOfsnacks + favoriteSnake;
+console.log(snakeResult);
