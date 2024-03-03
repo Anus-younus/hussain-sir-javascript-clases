04c6e41fecb5c647e257ff73be528255cc0c523d
Anus Younus <AnusYounus254@gmail.com>
2024-03-03 15:54:03 +0500

    hello world app added

---
+++ Desktop/javascript/class 2/app.js
@@ -0,0 +1,33 @@
+/*alert("Welcome");
+console.log(5 + 5 - 2 * 5);
+
+alert("04323592348");
+console.log(009348830883983);*/
+
+// document.write("Halo world");
+// alert("Hello World");
+/* alert("Welcome");
+alert("Error! Please enter a valid password");*/
+
+// alert("Welcome to JS \n Happy Coding");
+
+// document.write("Welcome to js Land <br /><h3>Happy Coding</h3>");
+/*let userName, fatherName, rollNumber;
+userName = "Anus";
+fatherName = "Younus";
+rollNumber = "4045879";
+console.log(userName, fatherName, rollNumber);
+*/
+
+/*let userName = "Anus";
+let fatherName = "Younus";
+console.log(userName, fatherName);*/
+/*
+let userName = "Anus",
+  fatherName = "Younus";
+console.log(userName, fatherName);*/
+
+let userName = "Anas"
+let greeting = "Eid Mubarak"
+
+console.log(userName + " " + greeting)
